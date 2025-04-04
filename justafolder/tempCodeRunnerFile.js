837248d39e9e8cdf324fb7f95809f57a97d3baf9
mariam kharazishvili <kharazishvilimari2004@gmail.com>
2025-04-04 06:09:28 +0400

    branch

---
+++ justafolder/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+let student ={
+//     firstName:"mariam",
+//     lastName:"kharazishvili",
+//     age:21,
+//     scores:[ 4,7,5,3,2],
+//     fullName: function(){
+//         return this.firstName + this.lastName
+//     }
+// }
